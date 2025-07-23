fdb5b79e451076d1b4c989ac6158f8ee27f1bf63
Willians Freitas <williansvpf@gmail.com>
2025-07-23 19:38:33 -0300

    Adicionar classe AppDbContext para configuração do Entity Framework

---
+++ Contexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudoEntityFramework.Contexts;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=localhost,1433;Database=master;User Id=sa;Password=Root@1234;TrustServerCertificate=True;");
+    }
+}
