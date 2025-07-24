04e111eac8eea1576401fe1938a4e49c82f3b5d6
Willians Freitas <williansvpf@gmail.com>
2025-07-24 19:09:56 -0300

    Refatorar método OnModelCreating para aplicar configuração de ClienteTypeConfiguration usando ClienteEntityConfig

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -1,3 +1,4 @@
+using EstudoEntityFramework.EntityConfigs;
 using EstudoEntityFramework.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,29 +13,8 @@ public class AppDbContext : DbContext
         options.UseSqlServer("Server=localhost,1433;Database=EstudoEFCore;User Id=sa;Password=Root@1234;TrustServerCertificate=True;"); //? string de conexão
     }
 
-    protected override void OnModelCreating(ModelBuilder builder) //? configura classe usando IEntityTypeConfiguration
+    protected override void OnModelCreating(ModelBuilder builder) //? chama a classe de configuração usando IEntityTypeConfiguration
     {
-        builder.Entity<ClienteTypeConfiguration>()
-            .ToTable("clientes"); //*define o nome da tabela
-
-        builder.Entity<ClienteTypeConfiguration>()
-            .Property(cliente => cliente.Id) //* define a propriedade
-            .HasColumnName("id") //* define o nome da coluna
-            .HasColumnType("int"); //* define o tipo de dado
-
-        builder.Entity<ClienteTypeConfiguration>()
-            .HasKey(cliente => cliente.Id); //* defina a chave primaria
-
-        builder.Entity<ClienteTypeConfiguration>()
-            .Property(cliente => cliente.Nome)
-            .IsRequired() //? indica que esse campo é obrigatório
-            .HasColumnName("nome")
-            .HasColumnType("nvarchar(100)");
-
-        builder.Entity<ClienteTypeConfiguration>()
-            .Property(cliente => cliente.Cpf)
-            .IsRequired()
-            .HasColumnName("cpf")
-            .HasColumnType("nvarchar(15)");
+        builder.ApplyConfiguration<ClienteTypeConfiguration>(new ClienteEntityConfig()); 
     }
 }
