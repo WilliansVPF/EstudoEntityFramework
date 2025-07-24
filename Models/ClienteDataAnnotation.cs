15e08e5e079ed0cd407471f51a20429fb272c255
Willians Freitas <williansvpf@gmail.com>
2025-07-24 18:54:15 -0300

    Adicionar classe ClienteDataAnnotation com configurações de Data Annotations

---
+++ Models/ClienteDataAnnotation.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EstudoEntityFramework.Models;
+
+//* configurando a classe usando Data Annotations
+
+[Table("clientes")] //? nomeia a tabela. Caso não seja indicado um nome, a tabela tera o mesmo nome da classe no plural (Cliente = Clientes)
+public class ClienteDataAnnotation
+{
+    [Key] //? Indica que essa propriedade representa uma chave primaria. Por padrão, propriedades com nome Id ou NomeDaClasseId, são definidas automaticamente como primarias
+    [Column("id")] //? Define um nome para a coluna. Caso não seja definido, a coluna tera o nome da propriedade
+    public int Id { get; set; }
+
+    [Column("nome")]
+    [MaxLength(100)] //? Define quantidade maxima de caracters. Pode ser usado para validação
+    public string Nome { get; set; } = string.Empty;
+
+    [Column("cpf" , TypeName = "nvarchar(15)")] //? TypeName define qual vai ser o tipo do dado
+    public string Cpf { get; set; } = string.Empty;
+}
