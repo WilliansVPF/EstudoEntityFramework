6e751b09ee2cecf73c9fe35f8d8608807a1dade6
Willians Freitas <williansvpf@gmail.com>
2025-07-24 18:54:37 -0300

    Adicionar classe ClienteTypeConfiguration para configuração do Type Configuration

---
+++ Models/ClienteTypeConfiguration.cs
@@ -0,0 +1,10 @@
+namespace EstudoEntityFramework.Models;
+
+//* configurando a classe usando IEntityTypeConfiguration (DbContext)
+
+public class ClienteTypeConfiguration
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
+}
