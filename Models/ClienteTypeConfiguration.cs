87bb8ed81c7e0212d8eff07fef20c407bab1e888
Willians Freitas <williansvpf@gmail.com>
2025-07-25 18:15:49 -0300

    Adicionar propriedade de navegação Endereco na classe ClienteTypeConfiguration

--- Models/ClienteTypeConfiguration.cs
+++ Models/ClienteTypeConfiguration.cs
@@ -7,4 +7,6 @@ public class ClienteTypeConfiguration
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string Cpf { get; set; } = string.Empty;
+
+    public Endereco Endereco { get; set; } = null!; //? propriedade de navegação para poder acessar endereço pela classe cliente
 }
