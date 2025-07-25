34ed607f4c7dac1dba0864245586dc4ef1e5dc49
Willians Freitas <williansvpf@gmail.com>
2025-07-25 19:25:00 -0300

    Adicionar a classe Pedido com propriedades e relacionamento com ClienteTypeConfiguration

---
+++ Models/Pedido.cs
@@ -0,0 +1,12 @@
+namespace EstudoEntityFramework.Models;
+
+public class Pedido
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public DateTime Data { get; set; }
+
+    public int ClienteTypeConfigurationId { get; set; }
+    public ClienteTypeConfiguration Cliente { get; set; } = null!;
+
+}
