00dc5c2600f1a4e54be5dbe8eb9aaf072977ff2c
Willians Freitas <williansvpf@gmail.com>
2025-07-24 23:02:45 -0300

    Adicionar migrações para as tabelas clientesDataAnnotation e clientesTypeConfiguration

---
+++ Migrations/20250724221021_CreateTableCliente.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudoEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientesDataAnnotation",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    cpf = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientesDataAnnotation", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "clientesTypeConfiguration",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    cpf = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientesTypeConfiguration", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clientesDataAnnotation");
+
+            migrationBuilder.DropTable(
+                name: "clientesTypeConfiguration");
+        }
+    }
+}
