4bc6e05be796416d899922f117c0c7f1675623dd
Willians Freitas <williansvpf@gmail.com>
2025-07-25 19:27:14 -0300

    migrations

---
+++ Migrations/20250725145053_CreateTableEnderecos.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudoEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableEnderecos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "enderecos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    estado = table.Column<string>(type: "nvarchar(2)", nullable: false),
+                    cidade = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    bairro = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    logradouro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    numero = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    clienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enderecos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_enderecos_clientesTypeConfiguration_clienteId",
+                        column: x => x.clienteId,
+                        principalTable: "clientesTypeConfiguration",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enderecos_clienteId",
+                table: "enderecos",
+                column: "clienteId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "enderecos");
+        }
+    }
+}
---
+++ Migrations/20250725215426_CreateTablePedidos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudoEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTablePedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "pedido",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClienteTypeConfigurationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pedido", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pedido_clientesTypeConfiguration_ClienteTypeConfigurationId",
+                        column: x => x.ClienteTypeConfigurationId,
+                        principalTable: "clientesTypeConfiguration",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pedido_ClienteTypeConfigurationId",
+                table: "pedido",
+                column: "ClienteTypeConfigurationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pedido");
+        }
+    }
+}
