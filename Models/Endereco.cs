68e523883ab65651a973fd33a4f5fe57439744d4
Willians Freitas <williansvpf@gmail.com>
2025-07-25 18:15:30 -0300

    Adicionar a classe Endereco com propriedades básicas e referência a ClienteTypeConfigurationId

---
+++ Models/Endereco.cs
@@ -0,0 +1,14 @@
+namespace EstudoEntityFramework.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Logradouro { get; set; } = string.Empty;
+        public string Numero { get; set; } = string.Empty;
+
+        public int ClienteTypeConfigurationId { get; set; }
+	}
+}
