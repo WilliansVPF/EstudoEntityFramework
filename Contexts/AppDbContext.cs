f3341c9b787e97701c7b8994e7e5e11eea20c3e2
Willians Freitas <williansvpf@gmail.com>
2025-07-25 19:26:50 -0300

    Adicionar configuração da entidade Pedido no contexto do banco de dados

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -6,9 +6,10 @@ namespace EstudoEntityFramework.Contexts;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<ClienteDataAnnotation> ClienteDataAnnotations => Set<ClienteDataAnnotation>(); //? indica para o contexto as classes que representam as tabelas
+    public DbSet<ClienteDataAnnotation> ClienteDataAnnotations => Set<ClienteDataAnnotation>(); //TODO indica para o contexto as classes que representam as tabelas
     public DbSet<ClienteTypeConfiguration> ClienteTypeConfigurations => Set<ClienteTypeConfiguration>();
-    public DbSet<Endereco> Enderecos => Set<Endereco>(); //? como a tabela de cliente ja faz referencia a endereço, não é obrigatorio essa declaração, porem, para fazer busca direto na tabela de endereço, é necessaria essa declaração
+    public DbSet<Endereco> Enderecos => Set<Endereco>(); //TODO como a tabela de cliente ja faz referencia a endereço, não é obrigatorio essa declaração, porem, para fazer busca direto na tabela de endereço, é necessaria essa declaração
+    public DbSet<Pedido> Pedidos => Set<Pedido>();
     protected override void OnConfiguring(DbContextOptionsBuilder options) //? configuração de conexão
     {
         options.UseSqlServer("Server=localhost,1433;Database=EstudoEFCore;User Id=sa;Password=Root@1234;TrustServerCertificate=True;"); //? string de conexão
@@ -18,5 +19,6 @@ public class AppDbContext : DbContext
     {
         builder.ApplyConfiguration<ClienteTypeConfiguration>(new ClienteEntityConfig());
         builder.ApplyConfiguration<Endereco>(new EnderecoEntityConfig());
+        builder.ApplyConfiguration<Pedido>(new PedidoEntityConfig());
     }
 }
