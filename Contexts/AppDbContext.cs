3a201bfa00536366e5ea4a57bc13959cdb06604e
Willians Freitas <williansvpf@gmail.com>
2025-07-26 20:09:29 -0300

    Adicionar configuração da entidade Produto no contexto do banco de dados

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -10,6 +10,7 @@ public class AppDbContext : DbContext
     public DbSet<ClienteTypeConfiguration> ClienteTypeConfigurations => Set<ClienteTypeConfiguration>();
     public DbSet<Endereco> Enderecos => Set<Endereco>(); //TODO como a tabela de cliente ja faz referencia a endereço, não é obrigatorio essa declaração, porem, para fazer busca direto na tabela de endereço, é necessaria essa declaração
     public DbSet<Pedido> Pedidos => Set<Pedido>();
+    public DbSet<Produto> Produtos => Set<Produto>();
     protected override void OnConfiguring(DbContextOptionsBuilder options) //? configuração de conexão
     {
         options.UseSqlServer("Server=localhost,1433;Database=EstudoEFCore;User Id=sa;Password=Root@1234;TrustServerCertificate=True;"); //? string de conexão
@@ -20,5 +21,6 @@ public class AppDbContext : DbContext
         builder.ApplyConfiguration<ClienteTypeConfiguration>(new ClienteEntityConfig());
         builder.ApplyConfiguration<Endereco>(new EnderecoEntityConfig());
         builder.ApplyConfiguration<Pedido>(new PedidoEntityConfig());
+        builder.ApplyConfiguration<Produto>(new ProdutoEntityConfig());
     }
 }
